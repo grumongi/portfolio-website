a719302db39210767ebb61728f3c2f5559a63bce
grumongi <gustaverumongi@gmail.com>
2024-11-07 14:50:07 -0700

    task-1.2

---
+++ simple-js-app/scripts.js
@@ -0,0 +1,5 @@
+let pokemons = [
+    {name: 'Balbusaur' , height: '0.5', type: ['grass', 'poison']},
+    {name:'Squirtle' , height: '0.5,', type: ['water', 'fire'] },
+    {name:'Raichu' , height: '0.8,', type: 'flying' }
+];
