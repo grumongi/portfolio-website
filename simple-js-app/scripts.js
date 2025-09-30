c030963f6c057556eb68ef2f2c7907a9d1d2524d
grumongi <gustaverumongi@gmail.com>
2025-09-30 17:05:00 -0500

    task 6.5: cleaning up files

--- simple-js-app/scripts.js
+++
@@ -1,5 +0,0 @@
-let pokemons = [
-    {name: 'Balbusaur' , height: '0.5', type: ['grass', 'poison']},
-    {name:'Squirtle' , height: '0.5,', type: ['water', 'fire'] },
-    {name:'Raichu' , height: '0.8,', type: 'flying' }
-];
